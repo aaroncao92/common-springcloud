c6c4eab5ddb60939fcce1841146d61051b1893f6
yeran.cao <aaroncaoyeran@qq.com>
2019-10-30 13:39:47 +0800

    gogogo

---
+++ springcloud/src/test/java/com/aarontroop/springcloud/consumer/SpringcloudConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarontroop.springcloud.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
